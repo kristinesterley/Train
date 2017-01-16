e41a4d467b1e9885c9b13dae6122393fb6aead4c
Kristin Esterley <Kristin@Kristins-MBP.attlocal.net>
2017-01-16 14:27:41 -0500

    complete update/comment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,9 @@
-  //Initialize Firebase
+
+  //Global Variables
   var current_key;
   var formMode = "add";
+
+    //Initialize Firebase
   var config = {
     apiKey: "AIzaSyC4Rgu03KTJMBm4N5Vy6msm4Dqa5VN7vWc",
     authDomain: "train-ca218.firebaseapp.com",
@@ -8,9 +11,12 @@
     storageBucket: "train-ca218.appspot.com",
     messagingSenderId: "185214098012"
   };
-
-
   firebase.initializeApp(config);
+
+  // calculateTime takes two parameters: first time and frequency
+  //using these values, calculateTime computes the next train arrival time
+  // and the number of minutes until it arrives and returns those two values in 
+  // an array
 
   function calculateTime(fTime, freq) {
 
@@ -36,6 +42,8 @@
 
   }
 
+// resetform clears the form fields and set the form title and mode to "add"
+
 function resetForm(){
   	$("#trainName").val('');
   	$("#destination").val('');
@@ -45,10 +53,15 @@ function resetForm(){
   	$(".trainForm").text("Add Train");
   	formMode = "add";
 }
-  var database = firebase.database().ref().child("Trains");
-  
 
-  	database.on("child_added", snap => {
+// get a handle to the database
+
+  var database = firebase.database().ref().child("Trains");
+ 
+// when a new train record is added as well as the window loads, get the 
+// data from the database and add it to the table
+
+  database.on("child_added", snap => {
   		var name = snap.child("Name").val();
   		var destination = snap.child("Destination").val();
   		var frequency = snap.child("Frequency").val();
@@ -59,14 +72,14 @@ function resetForm(){
   		var minAway = 1;
   		var timeCalc = [];
   		timeCalc = calculateTime(firstTime,frequency);
-  		// console.log(timeCalc[0] + " ," + timeCalc[1]);
+
 
   		$("#table_body").append("<tr class='key"+key+"'>"+
   			"<td id='n"+key+"'>" + name + "</td>"+
   			"<td id = 'd"+ key+"'>" + destination +"</td>"+
   			"<td id = 'f"+key+"' data-start="+ firstTime+">"+ frequency+ "</td>"+
-  			"<td>" + timeCalc[next] + "</td>"+
-  			"<td>" + timeCalc[minAway] +"<td>"+
+  			"<td id = 'a"+key+"'>" + timeCalc[next] + "</td>"+
+  			"<td id = 'm"+key+"'>" + timeCalc[minAway] +"<td>"+
   			"<button type='button' class='btn btn-default update' data-key="+key+">Update</button>" + 
   			"<button type='button' class='btn btn-default delete' data-key="+ key +" >Delete</button></td></tr>");
   	
@@ -74,9 +87,18 @@ function resetForm(){
 
   });
 
-  	database.on("child_removed", snap => {
+  // when a train record is removed from the database, delete it from the table
+
+  database.on("child_removed", snap => {
   		$(".key"+current_key).remove();
   });
+
+  // when the user clicks the submit button, first check if in add mode or otherwise
+  // in update mode.
+  // if adding, add the data to the database
+  // if updating, update the database and update the table in the display
+
+  // once done, reset the form to be empty and in add mode
 
   $("#submit").on("click",function(){
 
@@ -91,21 +113,43 @@ function resetForm(){
   	} 
   	else {
 
+      var uName = $("#trainName").val().trim();
+      var uDest = $("#destination").val().trim();
+      var uFirstTime = $("#firstTime").val().trim();
+      var uFreq = $("#frequency").val().trim();
+
   		var updateDatabase = firebase.database().ref().child("Trains/"+current_key);
   		updateDatabase.set({
-  			Name: $("#trainName").val().trim(),
-    		Destination: $("#destination").val().trim(),
-    		FirstTime: $("#firstTime").val().trim(),
-    		Frequency: $("#frequency").val().trim()
+  			Name: uName,
+    		Destination: uDest,
+    		FirstTime: uFirstTime,
+    		Frequency: uFreq
 
   		});
+      //refresh the row in the display to show new info ****
+      var next = 0;
+      var minAway = 1;
+      var timeCalc = [];
+      timeCalc = calculateTime(uFirstTime,uFreq);
+
+      $("#n"+ current_key).text(uName);
+      $("#d"+ current_key).text(uDest);
+      $("#f"+ current_key).text(uFreq);
+      $("#f"+ current_key).attr("data-start",uFirstTime);
+      $("#a"+ current_key).text(timeCalc[next]);
+      $("#m"+ current_key).text(timeCalc[minAway]);
 
   	}
 
 
   	resetForm();
 
+
   });
+
+ // when user clicks the delete button, save off the key then 
+ // delete the record from the database. The database.on("child_removed")
+ // code will be kicked off, and in that code, the row is deleted from the display
 
   $(document).on("click",'.delete',function(){
   	current_key = $(this).attr("data-key");
@@ -125,7 +169,8 @@ function resetForm(){
 
   });
 
-	$("#cancel").on("click",function(){
+  // if user clicks cancel, clear the form and set to add mode
 
+	$("#cancel").on("click",function(){
 		resetForm();
 	});		
